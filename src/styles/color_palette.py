22c7f7529e3dcb3cbc5aa68c3eeeb5798dd58552
hugoromerito <hugoxvida@gmail.com>
2023-07-27 18:28:17 -0300

    Adiciona paleta de cores

---
+++ src/styles/color_palette.py
@@ -0,0 +1,29 @@
+# Author: Hugo de Souza de Queiroz (Romerito Queiroz)
+# Date: 2023-07-22
+# Title: Eleição Transparente
+# Description: Neighborhood voting report generation system
+# Version: 1.0.0
+# License: MIT License
+
+colors = {
+    'background_first_light': '#FFFFFF',
+    'background_first_dark': '#121212',
+    'background_second': '#1A2032',
+    'button_submit_light': '#2C35FF',
+    'button_submit_dark': '#9641FF',
+    'button_clear': '#d1313e',
+    'button_submit_hover_light': '#252EFF',
+    'button_submit_hover_dark': '#9137FF',
+    'button_clear_hover': '#CA2A2A',
+    'input_text': 'transparent',
+    'border_input_text_light': '#6066FF',
+    'border_input_text_dark': '#BB86FC',
+    'title_logo_light': '#252EFF',
+    'title_logo_dark': '#FFFFFF',
+    'label_light': '#000118',
+    'label_dark': '#FFFFFF',
+    'options_combobox_light': '#6066FF',
+    'options_combobox_dark': '#BB86FC',
+    'scrollbar_light': '#4D54FF',
+    'scrollbar_dark': '#B06FFF',
+}
